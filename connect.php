56768674ebc5ea57386443319a9684d07cb0891d
sz3papdav <144676937+sz3papdav@users.noreply.github.com>
2023-11-30 08:03:12 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$server = 'localhost';
+$db = 'diakok';
+$user = 'root';
+$pwd = '';
+
+try{
+    $connect = new PDO("mysql:host=$server;dbname=$db", $user, $pwd);
+}
+catch (Exception $e){
+    print($e -> getMessage());
+}
